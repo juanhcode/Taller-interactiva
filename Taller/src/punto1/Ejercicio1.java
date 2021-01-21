18cf6a4c0b49226863515b30fb94d312631bba8d
Juan Hoyos <64670953+juanhcode@users.noreply.github.com>
2021-01-21 17:21:34 -0500

    Punto 1

--- Taller/src/punto1/Ejercicio1.java
+++ Taller/src/punto1/Ejercicio1.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package punto1;
 
 import java.util.Scanner;
@@ -13,52 +8,51 @@ import java.util.Scanner;
  */
 public class Ejercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        //Sumar un valor
-        //Sacar residuo
-        // mover la posicion del numero
-        
         String mensaje;
-        String mensaje1;
-        
         String cifrado = "";
         Scanner consola = new Scanner(System.in);
         System.out.println("digite numero para cifrar: ");
         mensaje = consola.nextLine();
-        
-        for(int i = 0; i < mensaje.length();i++){
+
+        for (int i = 0; i < mensaje.length(); i++) {
             int aux = mensaje.charAt(i) - '0';
             aux += 8;
             aux %= 9;
+
             cifrado += aux;
-            
+
         }
         mensaje = cifrado.charAt(2) + "";
         mensaje += cifrado.charAt(3) + "";
         mensaje += cifrado.charAt(0) + "";
         mensaje += cifrado.charAt(1) + "";
         System.out.println("numero cifrado: " + mensaje);
-        
-        
-        System.out.println("digite numero para Descifrar: ");
-        mensaje1 = consola.nextLine();
-        for(int i = 0; i < mensaje1.length();i++){
-            int aux = mensaje1.charAt(i) - '0';
-            aux -= 8;
-            aux += 9;
-            cifrado += aux;
-            
+
+        String mensaje2;
+        String decifrado2 = "";
+        Scanner consola2 = new Scanner(System.in);
+        System.out.println("digite numero para decifrar: ");
+        mensaje2 = consola2.nextLine();
+
+        for (int i = 0; i < mensaje2.length(); i++) {
+            int aux2 = mensaje2.charAt(i) - '0';
+
+            int suma = aux2 + 8;
+            int decodificado = aux2 + 9;
+            int residuo = aux2 % 9;
+            int decodificado2 = residuo + decodificado - suma;
+
+            decifrado2 += String.valueOf(decodificado2);
+
         }
-        mensaje1 = cifrado.charAt(0) + "";
-        mensaje1 += cifrado.charAt(1) + "";
-        mensaje1 += cifrado.charAt(2) + "";
-        mensaje1 += cifrado.charAt(3) + "";
-        System.out.println("numero cifrado: " + mensaje1);
-       
-        
+
+        mensaje2 = decifrado2.charAt(2) + "";
+        mensaje2 += decifrado2.charAt(3) + "";
+        mensaje2 += decifrado2.charAt(0) + "";
+        mensaje2 += decifrado2.charAt(1) + "";
+        System.out.println("numero decifrado: " + mensaje2);
+
     }
-    
+
 }
